619603c4cd18a3627f326431f5c802e6c7a71c26
jtb <james.baker@vanderbilt.edu>
2025-04-24 09:24:26 -0500

    test(updated-the-integration-test): Updated integration test to use simulated IBD data as test data
    Found a way to simulate IBD data using msprime. This simulated data is now the input for the test cases in drive. Also updated the pytest package so it is a non dev dependency that way all individuals will have it installed. And updated syntax for integration test because it was very out of date.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,37 +1,38 @@
-from typer.testing import CliRunner
+
 import pytest
 import sys
+import pandas as pd
 
-sys.path.append("./drive")
+sys.path.append("./src")
 
-from drive import app
+from drive import drive
 
-runner = CliRunner()
+
 
 # @pytest.mark.integtest
 # def test_drive_full_run():
 #     assert 1==1
-
-
-@pytest.mark.integtest
-def test_drive_full_run():
-    result = runner.invoke(
-        app,
+@pytest.fixture()
+def system_args(monkeypatch):
+    monkeypatch.setattr("sys.argv", 
         [
-            "-i",
-            "./tests/test_kcne_inputs/biovu_longQT_EUR_chr21.ibd.gz",
+            "drive", "-i",
+            "./tests/test_inputs/simulated_ibd_test_data_v2_chr20.ibd.gz",
             "-f",
             "hapibd",
             "-t",
-            "21:35818986-35884508",
+            "20:4666882-4682236",
             "-o",
             "./tests/test_output/integration_test_results",
             "-m",
             "3",
-            "-vv",
-            "--no-recluster",
-        ],
-    )
+            "--recluster"
+            ])
+
+@pytest.mark.integtest
+def test_drive_full_run_no_phenotypes(system_args):
+    drive.main()
+
     # result = runner.invoke(
     #     app,
     #     [
@@ -39,5 +40,17 @@ def test_drive_full_run():
     #     ],
     #     color=True
     # )
-    print(result)
-    assert result.exit_code == 0
+    # we need to make sure the output was properly formed
+    output = pd.read_csv("./tests/test_output/integration_test_results.drive_networks.txt", sep="\t")
+    # list of errors to keep
+    errors = []
+
+    # list of columns it should have
+    expected_colnames = ["clstID", "n.total", "n.haplotype", "true.positive.n", "true.positive", "falst.postive", "IDs", "ID.haplotype"]
+
+    if not output.shape == (165, 8):
+        errors.append(f"Expected the output to have 165 rows and 8 columns instead it had {output.shape[0]} rows and {output.shape[1]}")
+    if [col for col in output.columns if col not in expected_colnames]:
+        errors.append(f"Expected the output to have the columns: {','.join(expected_colnames)}, instead these columns were found: {','.join(output.columns)}")
+        
+    assert not errors, "errors occured:\n{}".format("\n".join(errors))
