03d9e92fb78c329c9a6ef35459394e4ab6a00b90
jtb <james.baker@vanderbilt.edu>
2025-04-24 11:12:04 -0500

    test(test_integration.py): Added a phenotype file and a test to make sure code worked with the phenotype file
    ADded a phenotype file with 3 different synthetic phenotypes and then added an integration test that uses the -c flag to make sure the code would run with phenotype files.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,4 +1,5 @@
 
+import itertools
 import pytest
 import sys
 import pandas as pd
@@ -13,24 +14,50 @@ from drive import drive
 # def test_drive_full_run():
 #     assert 1==1
 @pytest.fixture()
-def system_args(monkeypatch):
+def system_args_no_pheno(monkeypatch):
     monkeypatch.setattr("sys.argv", 
         [
-            "drive", "-i",
+            "drive", 
+            "-i",
             "./tests/test_inputs/simulated_ibd_test_data_v2_chr20.ibd.gz",
             "-f",
             "hapibd",
             "-t",
             "20:4666882-4682236",
             "-o",
-            "./tests/test_output/integration_test_results",
+            "./tests/test_output/integration_test_results_no_pheno",
             "-m",
             "3",
-            "--recluster"
+            "--recluster",
+            "--log-filename",
+            "integration_test_results_no_pheno.log"
+            ])
+    
+@pytest.fixture()
+def system_args_with_pheno(monkeypatch):
+    monkeypatch.setattr("sys.argv", 
+        [
+            "drive", 
+            "-i",
+            "./tests/test_inputs/simulated_ibd_test_data_v2_chr20.ibd.gz",
+            "-f",
+            "hapibd",
+            "-t",
+            "20:4666882-4682236",
+            "-o",
+            "./tests/test_output/integration_test_results_with_pheno",
+            "-m",
+            "3",
+            "-c",
+            "./tests/test_inputs/test_phenotype_file_withNAs.txt",
+            "--recluster",
+            "--log-file",
+            "integration_test_results_with_pheno.log"
             ])
 
+
 @pytest.mark.integtest
-def test_drive_full_run_no_phenotypes(system_args):
+def test_drive_full_run_no_phenotypes(system_args_no_pheno):
     drive.main()
 
     # result = runner.invoke(
@@ -41,7 +68,7 @@ def test_drive_full_run_no_phenotypes(system_args):
     #     color=True
     # )
     # we need to make sure the output was properly formed
-    output = pd.read_csv("./tests/test_output/integration_test_results.drive_networks.txt", sep="\t")
+    output = pd.read_csv("./tests/test_output/integration_test_results_no_pheno.drive_networks.txt", sep="\t")
     # list of errors to keep
     errors = []
 
@@ -49,6 +76,46 @@ def test_drive_full_run_no_phenotypes(system_args):
     expected_colnames = ["clstID", "n.total", "n.haplotype", "true.positive.n", "true.positive", "falst.postive", "IDs", "ID.haplotype"]
 
     if not output.shape == (165, 8):
+        errors.append(f"Expected the output to have 165 rows and 8 columns instead it had {output.shape[0]} rows and {output.shape[1]}")
+    if [col for col in output.columns if col not in expected_colnames]:
+        errors.append(f"Expected the output to have the columns: {','.join(expected_colnames)}, instead these columns were found: {','.join(output.columns)}")
+        
+    assert not errors, "errors occured:\n{}".format("\n".join(errors))
+
+
+@pytest.mark.integtest
+def test_drive_full_run_with_phenotypes(system_args_with_pheno):
+    drive.main()
+
+    # result = runner.invoke(
+    #     app,
+    #     [
+    #         "--help"
+    #     ],
+    #     color=True
+    # )
+    # we need to make sure the output was properly formed
+    output = pd.read_csv("./tests/test_output/integration_test_results_with_pheno.drive_networks.txt", sep="\t")
+
+
+
+    # list of errors to keep
+    errors = []
+
+    #lets read in the header of the phenotype file so that we can form the additional columns
+    with open("./tests/test_inputs/test_phenotype_file_withNAs.txt", "r") as pheno_input:
+        grid_col, pheno1, pheno2, pheno3 = pheno_input.readline().strip().split("\t")
+        
+        col_combinations = list(itertools.product([pheno1,pheno2, pheno3], ["_case_count_in_network", "cases_in_network", "_excluded_count_in_network", "excluded_in_network", "_pvalue"]))
+
+        phenotype_cols = ["min_pvalue", "min_phenotype", "min_phenotype_description"] + ["".join(val) for val in col_combinations]
+
+
+
+    # list of columns it should have
+    expected_colnames = ["clstID", "n.total", "n.haplotype", "true.positive.n", "true.positive", "falst.postive", "IDs", "ID.haplotype"] + phenotype_cols
+
+    if not output.shape == (165, 26):
         errors.append(f"Expected the output to have 165 rows and 8 columns instead it had {output.shape[0]} rows and {output.shape[1]}")
     if [col for col in output.columns if col not in expected_colnames]:
         errors.append(f"Expected the output to have the columns: {','.join(expected_colnames)}, instead these columns were found: {','.join(output.columns)}")
