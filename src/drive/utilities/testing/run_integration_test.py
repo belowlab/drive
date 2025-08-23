48cb5535429aced661dde8b92e7c312544f4acd1
J.T. Baker <59581582+jtb324@users.noreply.github.com>
2025-08-22 20:59:02 -0500

    test(run_integration_test.py): updated test subcommand to look for integration test in 3 different places
    PDM install doesn't include the integration test in the platlib path. The proposed solution uses the platlib path for the pypi and pip installations and then uses either tests/test_integration.py or /app/tests/test_integration.py for PDM and docker installs, respectively. This behavior allows users to run some form of the drive utilities test command and makes usage more consistent. The test path for PDM still requires users to run the command from the home directory.

--- src/drive/utilities/testing/run_integration_test.py
+++ src/drive/utilities/testing/run_integration_test.py
@@ -9,11 +9,16 @@ from log import CustomLogger
 logger = CustomLogger.get_logger(__name__)
 
 
+def check_for_file(file_path: Path) -> bool:
+    return file_path.exists()
+
+
 def run_integration_test(args: argparse.Namespace) -> None:
     logger.info("Running integration test to ensure that DRIVE was installed correctly")
 
-    site_packages_path = Path(sysconfig.get_paths().get("platlib")) / "tests"
+    site_packages_path = sysconfig.get_paths().get("platlib")
 
+    # if the sites package doesn't exist then we need to tell the user.
     if site_packages_path is None:
         logger.fatal(
             "Was not able to find the site-packages directory in the virtualenv that DRIVE was installed into. This error is unexpected. Please report this error to the maintainers. Terminating program..."
@@ -21,14 +26,37 @@ def run_integration_test(args: argparse.Namespace) -> None:
         system_paths = ", ".join(sysconfig.get_paths())
         logger.debug(f"Current System paths are: {system_paths}")
         sys.exit(1)
+    else:
+        test_path = Path(site_packages_path) / "tests"
 
-    sys.path.append(site_packages_path)
+    possible_file_paths = [
+        test_path / "test_integration.py",
+        Path("tests/test_integration.py"),
+        Path("/app/tests/test_integration.py"),
+    ]
 
-    pytest.main(
-        [
-            "-v",
-            site_packages_path / "test_integration.py",
-            "-W",
-            "ignore::DeprecationWarning",
-        ]
-    )
+    test_ran = False
+
+    for path in possible_file_paths:
+        if check_for_file(path):
+            logger.info(
+                f"Found the integration test file: {path}. Running integration test."
+            )
+            pytest.main(
+                [
+                    "-v",
+                    str(path),
+                    "-W",
+                    "ignore::DeprecationWarning",
+                ]
+            )
+
+            test_ran = True
+            break
+    # if we don't find the test in the site package directory then we can test for it in different places
+    if not test_ran:
+        search_paths = ", ".join([str(path) for path in possible_file_paths])
+        logger.fatal(
+            f"Could not find the unit test script in any of the paths {search_paths}. Please ensure that one of these paths exist."
+        )
+        sys.exit(1)
