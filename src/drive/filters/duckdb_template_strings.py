0669d09a3c904c257ad0e48855e5b1af46615c0a
J.T. Baker <james.baker@vanderbilt.edu>
2026-08-03 15:09:20 -0500

    fix(duckdb-template-failed-for-IBD-data-from-other-sources): The columns were not being correctly passed by Duckdb for other inputs from IBD detection programs. Changed the logic so that hte program reads the first line of the file and constructs a list of names from the indices of each column. We still use the IbdFileIndices namedtuples to set the types of the appropriate objects
    N

--- src/drive/filters/duckdb_template_strings.py
+++ src/drive/filters/duckdb_template_strings.py
@@ -2,8 +2,29 @@ from pathlib import Path
 from drive.models import IbdFileIndices
 from .loci_filters import FilterProtocol
 from log import CustomLogger
+import gzip
+from contextlib import contextmanager
+from typing import IO
+from collections.abc import Generator
 
 logger = CustomLogger.get_logger(__name__)
+
+
+@contextmanager
+def open_ibdfile(file_path: Path) -> Generator[IO[str], None, None]:
+    """Context manager abstraction to open gzipped or raw text files.
+
+    Parameters
+    ----------
+    file_path : Path
+        Path to IBD segments file.
+    """
+    if file_path.suffix == ".gz":
+        with gzip.open(file_path, mode="rt", encoding="utf-8") as f:
+            yield f
+    else:
+        with open(file_path, mode="r", encoding="utf-8") as f:
+            yield f
 
 
 class DuckdbTemplate:
@@ -19,6 +40,21 @@ class DuckdbTemplate:
         self.filter = filterObj
         self.indices = indices
         self.min_cm = min_cm
+
+    def sniff_columns(self) -> list[str]:
+        """parse the first line of the file to determine how many columns are
+        in the file
+
+        Returns
+        -------
+        list[str]
+            returns a list of column names. The IBD files generally don't have
+            a header so this will just pad the word 'column' with the number
+            indice
+        """
+        with open_ibdfile(self.ibd_file) as ibd_fh:
+            first_line = ibd_fh.readline().split("\t")
+        return [f"column{i}" for i, _ in enumerate(first_line)]
 
     def get_network_filter(self, add_sample_filter: bool) -> str:
         """generate the SQL query for when we want to filter the IBD file for the network analysis. Here we are just filtering the file on the following conditions: 1) samples either overlap or contain the locus of interest, 2) segments are longer than a minimum threshold, and 3) if we want to keep the files (This 3rd point can be turned on or off)
@@ -56,7 +92,8 @@ class DuckdbTemplate:
             '{self.ibd_file}',
             delim='\t',
             header=False,
-            columns={{
+            names={self.sniff_columns()},
+            types={{
                 '{self.indices.id1_indx}':'VARCHAR',
                 '{self.indices.hap1_indx}':'VARCHAR',
                 '{self.indices.id2_indx}':'VARCHAR',
@@ -65,7 +102,6 @@ class DuckdbTemplate:
                 '{self.indices.str_indx}':'BIGINT',
                 '{self.indices.end_indx}':'BIGINT',
                 '{self.indices.cM_indx}':'DOUBLE'
-
             }}
         ) as t
         WHERE
